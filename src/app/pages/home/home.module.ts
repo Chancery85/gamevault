1969ba8ff9e7e18aaef75721b1e488bd4b86d211
cpercival <cpercival@decentrix.net>
2020-10-06 20:36:17 -0600

    updating nav and element structure

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { HomeModuleRouting } from "./home.module.routing";
+import { GameItemComponent } from './game-item/game-item.component';
+import { GameEditComponent } from './game-item/game-edit/game-edit.component';
 
 
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [HomeComponent, GameItemComponent, GameEditComponent],
   imports: [
     CommonModule,
     HomeModuleRouting
